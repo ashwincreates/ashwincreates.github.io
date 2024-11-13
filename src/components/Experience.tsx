515467c9511a92dacca334ffc199d01457e09703
ashwincreates <ashwin200113@gmail.com>
2024-11-14 00:20:04 +0530

    fix

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -17,7 +17,7 @@ function Experience() {
   return (
     <section className="min-h-[500px] px-6 md:px-24 space-y-20">
       <h3 className="text-stone-300 font-display text-xl">Experience</h3>
-      <ul className="text-stone-300 space-y-12 space-x-0 md:space-y-0 space-x-12 md:flex">
+      <ul className="text-stone-300 space-y-12 space-x-0 md:space-y-0 gap-12 md:flex">
         {experience.map((exp) => (
           <li key={exp.company} className="pl-8 relative after:absolute after:h-2 after:w-2 after:left-2 after:top-2  after:bg-stone-500 after:rotate-45">
             <h4 className="font-display text-lg">{exp.company}</h4>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,27 +5,32 @@ import NavLink from "next/link";
 import { routes } from "../routes";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 import { useMotionValueEvent, useScroll } from "framer-motion";
 import { useState } from "react";
 
 function NavBar() {
   const pathname = usePathname();
-  const [isTop, setIsTop] = useState(true)
-  const scroll = useScroll()
+  const [isTop, setIsTop] = useState(true);
+  const scroll = useScroll();
 
-  useMotionValueEvent(scroll.scrollYProgress, 'change', (prev) => {
-    setIsTop(prev > 0 ? false: true)
-  })
+  useMotionValueEvent(scroll.scrollYProgress, "change", (prev) => {
+    setIsTop(prev > 0 ? false : true);
+  });
 
   return (
-    <nav className={cn("bg-none md:bg-stone-900/60 fixed top-4 right-4 md:bottom-4 left-4 rounded-md  md:w-[64px] z-10 overflow-hidden", !isTop && "bg-stone-900/60")}>
-      <ul className={cn("px-3 py-2 md:p-2 justify-between md:justify-start flex md:flex-col gap-6 items-center", !isTop && "backdrop-blur-sm")}>
+    <nav
+      className={cn(
+        "bg-none md:bg-stone-900/60 fixed top-4 right-4 md:bottom-4 left-4 rounded-md  md:w-[64px] z-10 overflow-hidden",
+        !isTop && "bg-stone-900/60"
+      )}
+    >
+      <ul
+        className={cn(
+          "px-3 py-2 md:p-2 justify-between md:justify-start flex md:flex-col gap-6 items-center",
+          !isTop && "backdrop-blur-sm"
+        )}
+      >
         <li className="mb-2 mt-1 md:my-2">
           <a href="/">
             <Logo />
@@ -64,24 +69,22 @@ function NavBar() {
             side="top"
             className="border-stone-700 flex flex-col gap-4"
           >
-                <NavLink
-                  style={{ textDecoration: "none" }}
-                  href={'/'}
+            <NavLink style={{ textDecoration: "none" }} href={"/"}>
+              <SheetClose>
+                <h4
+                  className={cn(
+                    "text-stone-400 hover:text-stone-50 font-display text-lg",
+                    pathname == "/" ? "text-stone-50" : ""
+                  )}
                 >
-                  <SheetClose>
-                    <h4
-                      className={cn(
-                        "text-stone-400 hover:text-stone-50 font-display text-lg",
-                        pathname == '/' ? "text-stone-50" : ""
-                      )}
-                    >
-                      Home
-                    </h4>
-                  </SheetClose>
-                </NavLink>
+                  Home
+                </h4>
+              </SheetClose>
+            </NavLink>
             {routes.map((item) => {
               return (
                 <NavLink
+                  key={item.title}
                   style={{ textDecoration: "none" }}
                   target={item.type === "link" ? "_blank" : undefined}
                   href={item.route}
